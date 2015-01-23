23dfb7415d6cd111a37fb9a3282d17020531b485
Ben Alderfer <balderfe@purdue.edu>
2015-01-23 15:32:02 -0500

    Clean

--- public/javascripts/bubbles.js
+++ public/javascripts/bubbles.js
@@ -1,47 +1,23 @@
 var b;
 var bctx;
-var color = 1;
+var colors = ['#2c3e50', '#ff6666', '#f5ae42', '#444444'];
+var minBubbleR = 10;
+var maxBubbleR = 60;
+var nBubbles = 35;
+
+var clickEvent = false;
 var bubbleArray = [];
 var cursorX = -1000;
 var cursorY = -1000;
 var pagePosition = 0;
 var multiplier = 1;
 
-$(document).ready(function() {
-	b = document.getElementById('bubbles');
+var updateCursorLocation = function(e) {
+	cursorX = e.pageX;
+	cursorY = e.pageY;
+}
 
-	b.width = $('.body-wrapper').width();
-	b.height = $('.body-wrapper').height();
-
-	bctx = b.getContext('2d');
-
-	initBubbles();
-
-	$(window).resize(function(event) {
-		b.width = $('.body-wrapper').width();
-		b.height = $('.body-wrapper').height();
-	});
-
-	document.onmousemove = function(e) {
-		cursorX = e.pageX;
-		cursorY = e.pageY;
-	}
-
-	document.onclick = function(e) {
-		for (var i = 0; i < bubbleArray.length; i++) {
-			if (isNearbyBubble(i, cursorX, cursorY)) {
-				bubbleArray[i].popped = true;
-			}
-		}
-	}
-
-	// $(window).scroll(function(event) {
-	// 	pagePosition = $('body').scrollTop();
-	// 	console.log(pagePosition);
-	// });
-});
-
-var isNearbyBubble = function(i, x, y) {
+var isHovered = function(i, x, y) {
 	var distance = calculateDistance(x, y, bubbleArray[i].x, bubbleArray[i].y);
 	if (distance < bubbleArray[i].r) {
 		return true;
@@ -54,91 +30,120 @@ var calculateDistance = function(x1, y1, x2, y2) {
 }
 
 var getColor = function() {
-	var rand = Math.random();
-	if (color == 1) {
-		color++;
-		return '#2c3e50';
-	}
-	else if (color == 2) {
-		color++;
-		return '#ff6666';
-	}
-	else if (color == 3) {
-		color++;
-		return '#f5ae42';
-	}
-	else if (color == 4) {
-		color++;
-		return '#444444';
-	}
-	else {
-		color = 2;
-		return '#2c3e50';
-	}
+	var color = colors.shift();
+	colors.push(color);
+	return color;
 }
 
-var drawCircle = function(c, ctx, x, y, r, c) {
-	ctx.beginPath();
-	ctx.arc(x, y, r, 0, 2 * Math.PI, false);
-	ctx.closePath();
-	ctx.fillStyle = c;
-	ctx.fill();
+var drawBubble = function(x, y, radius, color, opacity) {
+	bctx.beginPath();
+	bctx.arc(x, y, radius, 0, 2 * Math.PI, false);
+	bctx.closePath();
+	bctx.fillStyle = color;
+	bctx.globalAlpha = opacity;
+	bctx.fill();
+}
+
+// Returns a random x coordinate with the specified portion of the window 
+var getXWithinField = function(width, offset) {
+	return b.width * (offset + (Math.random() * width));
+}
+
+// Returns a random buttons size
+var generateRadius = function() {
+	return Math.random() * (maxBubbleR - minBubbleR) + minBubbleR;
 }
 
 var initBubbles = function() {
-	var numberOfButtons = 35;
-	for (var i = 0; i < numberOfButtons; i++) {
-		if (i < numberOfButtons * 0.33) {
-			var x = (Math.random() * (b.width / 5)) + (0 * b.width / 5);
-			var r = Math.random() * 50 + 10;
+	b = document.getElementById('bubbles');
+	b.width = document.getElementById('bubbleWrapper').offsetWidth;
+	b.height = document.getElementById('bubbleWrapper').offsetHeight;
+
+	bctx = b.getContext('2d');
+
+	// Listeners
+	document.onmousemove = function(e) {
+		updateCursorLocation(e);
+	}
+
+	document.onclick = function(e) {
+		clickEvent = true;
+	}
+
+	for (var i = 0; i < nBubbles; i++) {
+		if (i < nBubbles * 0.33) {
+			var x = getXWithinField(0.2, 0);
+			var r = generateRadius();
 		}
-		else if (i < numberOfButtons * 0.66) {
-			var x = (Math.random() * (3 * b.width / 5)) + (1 * b.width / 5);			
-			var r = Math.random() * 50 + 10;
+		else if (i < nBubbles * 0.66) {
+			var x = getXWithinField(0.6, 0.2);			
+			var r = generateRadius();
 		}
 		else {
-			var x = (Math.random() * (b.width / 5)) + (4 * b.width / 5);
-			var r = Math.random() * 50 + 10;
+			var x = getXWithinField(0.2, 0.8);
+			var r = generateRadius();
 		}
-		var y = Math.random() * $(window).height();
+		var y = Math.random() * window.innerHeight;
 		var c = getColor();
 
 		var opacity = (Math.random() * 0.2) + 0.1;
 
-		bctx.globalAlpha = opacity;
+		drawBubble(x, y, r, c, opacity);
 
-		drawCircle(b, bctx, x, y, r, c);
 		bubbleArray.push({
 			x: x,
 			y: y,
 			r: r,
+			dr: 0,
 			c: c,
 			hover: false,
 			opacity: opacity,
-			dr: 0,
-			popped: false
+			popped: false,
+			smiley: false
 		});
 	}
 
-	var update = window.setInterval(function() { updateBubbles() }, 30);
+	var update = window.setInterval(function() {
+		updateBubbles();
+	}, 30);
 }
 
 var updateBubbles = function() {
-	// alert('updateBubbles');
 	bctx.clearRect(0, 0, b.width, b.height);
+
+	// Update for each bubble
 	for (var i = 0; i < bubbleArray.length; i++) {
-		if (i % 5 == 0) {
-			bubbleArray[i].y = bubbleArray[i].y - (0.9 * multiplier);
+
+		// If click and bubble is hovered
+		if (clickEvent && isHovered(i, cursorX, cursorY)) {
+			bubbleArray[i].popped = true;
 		}
-		if (i % 2 == 0) {
-			bubbleArray[i].y = bubbleArray[i].y - (0.3 * multiplier);
+
+		// If scrolled beyond 50px then increase speed multiplier
+		if (window.scrollY > 50) {
+			multiplier *= 1.005;
+			if (multiplier > 1000) {
+				multiplier = 1;
+			}
 		}
 		else {
+			multiplier = 1;
+		}
+
+		// Set various speeds for bubbles
+		if (i % 5 == 0) {
+			bubbleArray[i].y = bubbleArray[i].y - (1.5 * multiplier);
+		}
+		else if (i % 2 == 0) {
 			bubbleArray[i].y = bubbleArray[i].y - (0.9 * multiplier);
 		}
-		if (bubbleArray[i].y < -65 && $(window).scrollTop() < 10 && Math.random() < 0.001) {
-			multiplier = 1;
-			bubbleArray[i].y = $(window).height() + 65;
+		else {
+			bubbleArray[i].y = bubbleArray[i].y - (0.3 * multiplier);
+		}
+
+		// If bubble is off the top of the screen, then randomly respawn bubbles
+		if (bubbleArray[i].y < -65 && window.scrollY <= 10 && Math.random() < 0.001) {
+			bubbleArray[i].y = window.innerHeight + 10 + maxBubbleR;
 			bubbleArray[i].dr = 0;
 			bubbleArray[i].opacity = (Math.random() * 0.2) + 0.1; 
 		}
@@ -152,7 +157,7 @@ var updateBubbles = function() {
 				if (bubbleArray[i].opacity < 0) {
 					bubbleArray[i].opacity = 0;
 					bubbleArray[i].popped = false;
-					bubbleArray[i].y = $(window).height() + 65;
+					bubbleArray[i].y = -65;
 					bubbleArray[i].dr = 0;
 					bubbleArray[i].opacity = (Math.random() * 0.2) + 0.1; 
 				}
@@ -160,7 +165,7 @@ var updateBubbles = function() {
 		}
 		else {
 			// Hover effects
-			if (isNearbyBubble(i, cursorX, cursorY)) {
+			if (isHovered(i, cursorX, cursorY)) {
 				bubbleArray[i].hover = true;
 
 				// Step up dr
@@ -170,14 +175,6 @@ var updateBubbles = function() {
 				else {
 					bubbleArray[i].dr = bubbleArray[i].r / 2;
 				}
-
-				// Step down opacity
-				// if (bubbleArray[i].opacity > 0.5) {
-				// 	bubbleArray[i].opacity -= 0.09;
-				// }
-				// else {
-				// 	bubbleArray[i].opacity = 0.5;
-				// }	
 			}
 			else {
 				bubbleArray[i].hover = false;
@@ -188,23 +185,21 @@ var updateBubbles = function() {
 				}
 				else {
 					bubbleArray[i].dr = 0;
-				}
-
-				// Step up opacity
-				// if (bubbleArray[i].opacity < 1) {
-				// 	bubbleArray[i].opacity += 0.09;
-				// }
-				// else {
-				// 	bubbleArray[i].opacity = 1;
-				// }			
+				}		
 			}
+		}
+
+		if (bubbleArray[i].opacity == 0) {
+			bubbleArray[i].smiley = true;
+			bubbleArray[i].popped = false;
+			bubbleArray[i].opacity = (Math.random() * 0.2) + 0.1;
 		}
 
 		// Fade if necessary
 		bctx.globalAlpha = bubbleArray[i].opacity;
 
-		// var scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
-
-		drawCircle(b, bctx, bubbleArray[i].x, bubbleArray[i].y, bubbleArray[i].r + bubbleArray[i].dr, bubbleArray[i].c);
+		drawBubble(bubbleArray[i].x, bubbleArray[i].y, bubbleArray[i].r + bubbleArray[i].dr, bubbleArray[i].c);
 	}
+
+	clickEvent = false;
 }
