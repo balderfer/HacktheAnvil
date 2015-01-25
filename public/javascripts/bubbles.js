30da509d1df3c1b3895c1e40a09bc85b4b4d0650
Ben Alderfer <balderfe@purdue.edu>
2015-01-25 14:49:59 -0500

    Remove scrollY

--- public/javascripts/bubbles.js
+++ public/javascripts/bubbles.js
@@ -12,6 +12,8 @@ var cursorX = -1000;
 var cursorY = -1000;
 var pagePosition = 0;
 var multiplier = 1;
+var wh = window.innerHeight;
+var ww = window.innerWidth;
 
 var updateCursorLocation = function(e) {
 	cursorX = e.pageX;
@@ -84,7 +86,7 @@ var initBubbles = function() {
 			var x = getXWithinField(0.2, 0.8);
 			var r = generateRadius();
 		}
-		var y = Math.random() * window.innerHeight;
+		var y = Math.random() * wh;
 		var c = getColor();
 
 		var opacity = (Math.random() * 0.2) + 0.1;
@@ -147,8 +149,8 @@ var updateBubbles = function(delta) {
 		}
 
 		// If bubble is off the top of the screen, then randomly respawn bubbles
-		if (bubbleArray[i].y < -65 && window.scrollY <= 10 && Math.random() < 0.001) {
-			bubbleArray[i].y = window.innerHeight + 10 + maxBubbleR;
+		if (bubbleArray[i].y < -65 && Math.random() < 0.001) {
+			bubbleArray[i].y = wh + 10 + maxBubbleR;
 			bubbleArray[i].dr = 0;
 			bubbleArray[i].opacity = (Math.random() * 0.2) + 0.1; 
 		}
